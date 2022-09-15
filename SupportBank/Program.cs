54466436ac82bedd4e80101da2db793d51d056c4
Jack Hughes <Jack.Hughes@softwire.com>
2022-09-15 12:03:32 +0100

    trying trying still very trying

--- SupportBank/Program.cs
+++ SupportBank/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace SupportBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to SupportBank, making people pay up since 2022!");
+            Console.WriteLine("========================================================");
+            FileReader cvsReader = new FileReader();
+
+            FileReader.ReadFile();
+
+        }
+    }
+}
